96ec308d3b9433a8715be85239ddac74b4ba1264
Ileis <gabrielileis07@gmail.com>
2021-08-23 09:24:35 -0300

    Junkfood

--- src/main/java/Espiral.java
+++ src/main/java/Espiral.java
@@ -1,2 +1,42 @@
 public class Espiral{
+
+    private String nomeProduto;
+    private int quantidade;
+    private double preco;
+
+    public Espiral(){
+        this.nomeProduto = " - ";
+    }
+
+    public void setNomeDoProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNomeDoProduto() {
+        return nomeProduto;
+    }
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void venderProduto(){
+        quantidade--;
+    }
+
+    public boolean isEmpty(){
+        return quantidade == 0;
+    }
 }
--- src/main/java/Maquina.java
+++ src/main/java/Maquina.java
@@ -1,46 +1,113 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Maquina {
 
+    private final int QTD_ESPIRAIS;
+    private final int MAXIMO_PRODUTOS;
+    private ArrayList<Espiral> espirais;
+    private double dinheiro;
+    private double saldo;
 
     public Maquina(int qtdEspirais, int maximoProdutos){
+        this.QTD_ESPIRAIS = qtdEspirais;
+        this.MAXIMO_PRODUTOS = maximoProdutos;
+        espirais = new ArrayList<Espiral>();
+        inicializaEspirais();
+    }
+
+    private void inicializaEspirais(){
+        for(int qtdEspirais = 0; qtdEspirais < QTD_ESPIRAIS; qtdEspirais++){
+            espirais.add(new Espiral());
+        }
     }
 
     public double getFaturamento() {
-        return 0;
+        return saldo;
     }
 
     public int getMaximoProdutos() {
-        return 0;
+        return MAXIMO_PRODUTOS;
     }
 
     public double getSaldoCliente() {
-        return 0;
+        return dinheiro;
     }
 
     public int getSizeEspirais(){
-        return 0;
+        return QTD_ESPIRAIS;
     }
 
     public Espiral getEspiral(int indice){
+        if(indiceValido(indice))
+            return espirais.get(indice);
         return null;
     }
 
     public boolean inserirDinheiro(double value){
+        if(value > 0) {
+            dinheiro += value;
+
+            return true;
+        }
         return false;
     }
 
     public double receberTroco(){
-        return 0;
+        double troco = dinheiro;
+        dinheiro = 0;
+        return troco;
     }
 
     public boolean alterarEspiral(int indice, String nome, int quantidade, double preco){
-        return true;
+        if(indiceValido(indice)){
+            if(quantidade <= MAXIMO_PRODUTOS){
+                espirais.get(indice).setQuantidade(quantidade);
+                espirais.get(indice).setPreco(preco);
+                espirais.get(indice).setNomeDoProduto(nome);
+
+                return true;
+            }
+            return false;
+        }
+        return false;
     }
 
     public boolean limparEspiral(int indice){
-        return true;
+        if(indiceValido(indice)){
+            espirais.set(indice, new Espiral());
+
+            return true;
+        }
+        return false;
     }
 
     public boolean vender(int indice){
+        if(indiceValido(indice)){
+            if(qtdSuficiente(indice) && dinheiroSuficiente(indice)){
+                espirais.get(indice).venderProduto();
+
+                saldo += espirais.get(indice).getPreco();
+                dinheiro -= espirais.get(indice).getPreco();
+
+                if (espirais.get(indice).isEmpty())
+                    limparEspiral(indice);
+
+                return true;
+            }
+        }
         return false;
+    }
+
+    private boolean indiceValido(int indice){
+        return (indice < QTD_ESPIRAIS && indice >= 0);
+    }
+
+    private boolean qtdSuficiente(int indice){
+        return espirais.get(indice).getQuantidade() > 0;
+    }
+
+    private boolean dinheiroSuficiente(int indice){
+        return (dinheiro >= espirais.get(indice).getPreco());
     }
 }
